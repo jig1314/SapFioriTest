16d60b4a1e2d962810a27b0d9124c0d21d45377d
Jonathan Gamble <jgambl24@students.kennesaw.edu>
2021-06-28 18:43:07 -0400

    Add project files.

---
+++ SapFioriTest/Client/NpmJS/src/index.js
@@ -0,0 +1,4 @@
+﻿import "@ui5/webcomponents/dist/Button.js"; // loads and defines ui5-button
+import "@ui5/webcomponents/dist/Table.js";
+import "@ui5/webcomponents/dist/TableColumn.js";
+import "@ui5/webcomponents/dist/TableRow.js";
